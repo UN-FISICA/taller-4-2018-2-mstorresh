37fd8277b4455f033a45e8fd9c2c2e13d3f80d15
mstorresh <mstorresh@unal.edu.co>
2018-10-04 21:15:56 -0500

    apenas se definieron las derivadas

--- funciones.py
+++ funciones.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
-def adelante(f,x,dx=10000):
+def adelante(f,x,dx=1e-8):
     return (f(x+dx)-f(x))/dx
-def central(f,x,dx=10000):
+def central(f,x,dx=1e-8):
     return (f(x+dx/2) - f(x-dx/2))/dx
-def extrapolada(f,x,dx=10000):
+def extrapolada(f,x,dx=1e-8):
     F1=(f(x+dx/2) - f(x-dx/2))/dx
     F2=(f(x+dx/4)-f(x-dx/4))/(dx/2)
     return (4*F2-F1)/3
+def segunda(f,x,dx=1e-8):
+    return (f(x+dx)+f(x-dx)-2*f(x))/(dx**2)
 print(adelante(np.sin,0))
