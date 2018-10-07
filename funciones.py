22070ef85fdc4370858461464e8eec9fe3aa1277
mstorresh <mstorresh@unal.edu.co>
2018-10-06 20:13:43 -0500

    solo falta la interpolacion

--- funciones.py
+++ funciones.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
+import math as m
 def adelante(f,x,dx=1e-8):
     return (f(x+dx)-f(x))/dx
 def central(f,x,dx=1e-8):
@@ -11,4 +12,22 @@ def extrapolada(f,x,dx=1e-8):
     return (4*F2-F1)/3
 def segunda(f,x,dx=1e-8):
     return (f(x+dx)+f(x-dx)-2*f(x))/(dx**2)
-print(adelante(np.sin,0))
+def newton(f,x1,error=1e-10): #se va a hacer con la derivada central
+    x2=x1-(f(x1)/central(f,x1))
+    while m.fabs(f(x2)-0)>error:
+        x1=x2
+        x2=x1-(f(x1)/central(f,x1))        
+    return (x2)
+def bisectriz(f,a,b,error=1e-10):
+    c=(a+b)/2.0
+    while m.fabs(f(c)-0)>error:
+        if f(c)*f(a)<0:
+            b=c
+            c=(a+b)/2.0
+        else:
+            a=c
+            c=(a+b)/2.0
+    return c
+def interpolacion(f,a,b,error=1e-10):
+    FX=f(a) +((f(b)-f(a))/(b-a)) *(x-a)
+    
