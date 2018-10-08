96a3225cf610df2f8f5f9787b29fb28d52805dd7
mstorresh <mstorresh@unal.edu.co>
2018-10-07 20:51:01 -0500

    finalizado, este es el programa que tiene definido las derivas y los metodos numericos

--- funciones.py
+++ funciones.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import math as m
-from scipy import optimize
 def adelante(f,x,dx=1e-8):
     return (f(x+dx)-f(x))/dx
 def central(f,x,dx=1e-8):
@@ -13,24 +12,34 @@ def extrapolada(f,x,dx=1e-8):
     return (4*F2-F1)/3
 def segunda(f,x,dx=1e-8):
     return (f(x+dx)+f(x-dx)-2*f(x))/(dx**2)
-def newton(f,x1,error=1e-10): #se va a hacer con la derivada central
-    x2=x1-(f(x1)/central(f,x1))
-    while m.fabs(f(x2)-0)>error:
-        x1=x2
-        x2=x1-(f(x1)/central(f,x1))        
+def newton(f,a,error=1e-10,max_iter=100): #se va a hacer con la derivada central
+    x2=a-(f(a)/central(f,a))
+    i = 1
+    while m.fabs(f(x2)-0)>error and i<max_iter:
+        a=x2
+        x2=a-(f(a)/central(f,a))  
+        i +=1
     return (x2)
-def bisectriz(f,a,b,error=1e-10):
+def bisectriz(f,a,b,error=1e-10,max_iter=100):
     c=(a+b)/2.0
-    while m.fabs(f(c)-0)>error:
+    i=1
+    while m.fabs(f(c)-0)>error and i<max_iter:
         if f(c)*f(a)<0:
             b=c
             c=(a+b)/2.0
         else:
             a=c
             c=(a+b)/2.0
+        i +=1    
     return c
-def newton_sp(f,x):
-    return optimize.newton(f,x)
-"""def interpolacion(f,a,b,error=1e-10):
-    FX=f(a)+((f(b)-f(a))/(b-a)) *(x-a)
-   """ 
+def interpolacion(f,a,b,error=1e-10,max_iter=100):
+    c=a-(f(a)*(b-a))/(f(b)-f(a))
+    i=1
+    while m.fabs(f(c)-0)>error and i<max_iter:
+        c=a-(f(a)*(b-a))/(f(b)-f(a))
+        if f(c)*f(a)<0:
+            b=c
+        else:
+           a=c
+        i +=1    
+    return c
